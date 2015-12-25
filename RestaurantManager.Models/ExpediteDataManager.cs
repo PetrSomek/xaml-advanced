ebe02c70f62e5275ed7879ed9836d37e4698c716
Petr Somek <petr.somek@seznam.cz>
2015-12-25 21:54:29 +0100

    iNotify implementation

---
+++ RestaurantManager.Models/ExpediteDataManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace RestaurantManager.Models
+{
+    public class ExpediteDataManager : DataManager
+    {
+        protected override void OnDataLoaded()
+        {
+            OnPropertyChanged("OrderItems");
+        }
+
+        public List<Order> OrderItems
+        {
+            get { return base.Repository.Orders; }
+        }
+    }
+}
---
+++ RestaurantManager.Models/OrderDataManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace RestaurantManager.Models
+{
+    public class OrderDataManager : DataManager
+    {
+        protected override void OnDataLoaded()
+        {
+            this.MenuItems = base.Repository.StandardMenuItems;
+
+            this.CurrentlySelectedMenuItems = new List<MenuItem>
+            {
+                this.MenuItems[3],
+                this.MenuItems[5]
+            };
+
+            OnPropertyChanged("MenuItems");
+            OnPropertyChanged("CurrentlySelectedMenuItems");
+        }
+
+
+
+        public List<MenuItem> MenuItems { get; set; }
+
+        public List<MenuItem> CurrentlySelectedMenuItems { get; set; }
+    }
+}
